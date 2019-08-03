f8408dc854c7f730ca129c8c90820fc59536c0e1
razp26 <>
2019-08-03 11:26:05 -0600

    Se agrega el enunciado del Laboratorio 1.
    Se agrega el enunciado del Laboratorio 1.

---
+++ Text Mining and Image Recognition/Laboratorio 1/Problema 2/problema2.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np 
+
+def derivada():
+    return 1
+
+derivada()
---
+++ Text Mining and Image Recognition/Laboratorio 1/problema1.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import sys
+
+def imgEQ(imgName):
+    # Leemos la imagen recibida como parametro
+    img = cv2.imread(imgName, 0)
+
+    # Obtenemos las dimensiones de la imagen, asi como los canales (RGB)
+    alto = img.shape[0]
+    ancho = img.shape[1]
+    imgSalida = np.zeros((alto,ancho))
+
+    histograma = np.zeros((256))
+    intensidad = range(0,256)
+
+    # Recorremos la imagen pixel por pixel
+    for i in range(alto):
+        for j in range(ancho):
+            pixel = img[i,j]
+            histograma[pixel]+=1 
+    
+    suma = np.sum(histograma)
+    histograma = histograma/suma
+    cdf = getCDF(histograma)
+
+    # Recorremos la imagen pixel por pixel
+    for i in range(alto):
+        for j in range(ancho):
+            pixel = img[i,j]
+            imgSalida[i,j] = 255 * cdf[pixel]
+
+    # Guardamos la imagen
+    cv2.imwrite("eq_image.jpg", imgSalida)
+    sys.exit()
+
+
+def getCDF(hist):
+    cumm = np.zeros((256))
+    sum = 0
+
+    for i in range(len(hist)):
+        sum = sum + hist[i]
+        cumm[i] = sum
+
+    return cumm
+
+
+imgStr = "test_image.jpg"
+imgEQ(imgStr)
+
+
