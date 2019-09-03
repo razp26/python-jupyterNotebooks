f4b500e0d855f973303da1bc7ad5250ee3dbf5c2
Chofocles <rodolfo.zea@users.noreply.github.com>
2019-09-02 22:36:38 -0700

    Derivada por diferencias finitas
    Se agrega ejemplo de cálculo de derivada por diferencias finitas.

--- Algoritmos en Ciencia de Datos/Laboratorio 1/Parte 1/1/diferencias_finitas.py
+++ Algoritmos en Ciencia de Datos/Laboratorio 1/Parte 1/1/diferencias_finitas.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 
 def getFormula(strEqn):
     # Initializing output string
@@ -23,7 +24,54 @@ def getFormula(strEqn):
 
     return '(' + strOutEqn + ')'
 
-formula = getFormula('3x^5 - 3x + 12')
-print(formula)
+def getDerivativeFormula(strEqn):
+
+    # Convert equation to standard form
+    strEqn = getFormula(strEqn)
+
+    # Generate first term
+    # f(x + 2h)
+    firstTerm = strEqn.replace('x', '(x+2*h)')
+
+    # Generate second term
+    # -4 f(x + h)
+    secondTerm = strEqn.replace('x', '(x+h)')
+    secondTerm = '-4*(' + secondTerm + ')'
+
+    # Generate third term
+    # +3 f(x)
+    thirdTerm = '+3*(' + strEqn + ')'
+
+    # Constructing derivative formula
+    derivative = firstTerm + secondTerm + thirdTerm
+    derivative = '-1*(' + derivative + ')/(2*h)'
+
+    return derivative
+
+
+def evalFormula(strEqn, variables):
+    # replacing variable names with its value
+    for index, row in variables.iterrows(): 
+        strEqn = strEqn.replace(row['name'], str(row['value']))
+
+    # evaluating function
+    return eval(strEqn)
+
+
+
+
+# Example
+# Getting derivative
+derivative = getDerivativeFormula('3x^5 - 3')
+
+# Evaluating derivative
+variables = [['x', -5], ['h', 0.000001]] 
+df_variables = pd.DataFrame(variables, columns = ['name', 'value']) 
+out = evalFormula(derivative, df_variables)
+
+# Printing result
+print(out)
+
+
 
 
