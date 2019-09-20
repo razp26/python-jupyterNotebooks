974728d44920b3fb0e479dd0e45ce26630ec1707
Chofocles <rodolfo.zea@users.noreply.github.com>
2019-09-20 08:00:25 -0700

    Proyecto
    Proyecto

--- Text Mining and Image Recognition/Hoja de trabajo 4/Problema 2/problema2.py
+++ Text Mining and Image Recognition/Hoja de trabajo 4/Problema 2/problema2.py
@@ -1,16 +1,15 @@
 import cv2 as cv 
-import sklearn
 from sklearn.cluster import KMeans
 
 # Reading image
-img = cv.imread('frutas.jpg')
+# img = cv.imread('frutas.jpg')
 
 # Reshaping image to a list of pixels
-img = img.reshape((img.shape[0] * img.shape[1], 3))
+# img = img.reshape((img.shape[0] * img.shape[1], 3))
 
 # Using k-means with our list of pixels
-clusters = KMeans(n_clusters = 6)
-clusters.fit(img)
+# clusters = KMeans(n_clusters = 6)
+# clusters.fit(img)
 
 # print(clusters.labels_)
-
+print('hola')
